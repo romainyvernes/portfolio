6df0812b3ddc786253c9d033d39da05425da89ec
Romain <romainyvernes@gmail.com>
2021-08-04 11:19:32 -0700

    Initial setup

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import '../styles/App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+    
+    </div>
+  );
+};
+
+export default App;
