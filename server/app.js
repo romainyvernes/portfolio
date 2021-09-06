9a02f108685ee4cd515e492d56727072b1abcd9d
Romain <romainyvernes@gmail.com>
2021-09-05 21:50:39 -0700

    Fix file structure in client, remove homepage key in client package.json

--- server/app.js
+++ server/app.js
@@ -8,17 +8,17 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.use('/', indexRouter);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
