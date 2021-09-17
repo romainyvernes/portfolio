4fbfcf42de06b2e9f1c214857eba28bc8b547bba
Romain <romainyvernes@gmail.com>
2021-09-17 16:25:17 -0700

    Contact form fixed

--- server/controllers/contactController.js
+++ server/controllers/contactController.js
@@ -1,38 +1,44 @@
 const nodemailer = require('nodemailer');
-const { google } = require('googleapis');
-const OAuth2 = google.auth.OAuth2;
+const { google } = require("googleapis");
 require('dotenv').config();
 
-// email server authentication setup
-const OAuth2Client = new OAuth2(
-  process.env.ID,
-  process.env.SECRET,
-  process.env.REDIRECT
+// gmail api authentication setup
+const OAuth2Client = new google.auth.OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  process.env.REDIRECT_URL
 );
 OAuth2Client.setCredentials({
   refresh_token: process.env.REFRESH
 });
 
-exports.sendEmail = async (req, res, next) => {
-  // obtain new token to access email server (TO FIX)
-  const accessToken = await OAuth2Client.getAccessToken();
-
-  // transporter setup
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'romainyvernesdev@gmail.com',
-      pass: 'Developer21@'
-    }
-  });
-  transporter.verify((err, success) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log('Server is ready to take messages');
-    }
-  });
+// get current access token
+const accessToken = OAuth2Client.getAccessToken();
   
+// transporter setup
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  auth: {
+    type: 'OAuth2',
+    user: process.env.USER_ID,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken: process.env.REFRESH,
+    accessToken
+  }
+});
+
+transporter.verify((err, success) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('Server is ready to take messages');
+  }
+});
+
+exports.sendEmail = (req, res, next) => {
   const email = {
     from: `${req.body.name} ${req.body.email}`,
     to: 'romainyvernes@gmail.com',
