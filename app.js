35a003646958fde5bf3fed633ff0bae0129a30a4
Romain <romainyvernes@gmail.com>
2021-09-18 15:03:50 -0700

    Create deployable version

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ app.use(cors());
 
 // This middleware informs the express application to serve our compiled React files
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', function (req, res) {
-      res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+      res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
   });
 };
 
