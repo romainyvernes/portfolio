bd763a89b5c8e904adc41db265b22e3dcb79aa8a
Romain <romainyvernes@gmail.com>
2021-09-20 14:46:50 -0700

    Fix router

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(cors());
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
   app.use(express.static(path.join(__dirname, 'client/build')));
 
-  app.get('*', function (req, res) {
+  app.get('/*', function (req, res) {
       res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
   });
 };
