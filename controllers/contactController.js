86c8f7c24aee2b024438a2034c5b613b417ff3a7
Romain <romainyvernes@gmail.com>
2021-10-20 18:44:07 -0700

    Update auth method

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -33,7 +33,9 @@ const transporter = nodemailer.createTransport({
 
 exports.sendEmail = (req, res, next) => {
   const transporter = nodemailer.createTransport({
-    service: 'Gmail',
+    service: 'gmail',
+    port: 8000,
+    secure: false,
     auth: {
       user: process.env.USER_ID,
       pass: process.env.PASSWORD
