5ec3acc7e3eeec663f33152cc892d96e6bc81480
Romain <romainyvernes@gmail.com>
2023-07-13 14:49:26 -0700

    Update about section

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -10,8 +10,7 @@ const About = () => {
         <FontAwesomeIcon icon={['far', 'user']} />
       </div>
       <p className="description">
-        Software engineer with roughly 3 years of experience developing innovative web apps and integrating robust APIs. Collaborative team player delivering high-quality solutions by analyzing needs, designing features, and staying updated on trends. Strong problem-solving and communication skills, committed to impactful results with attention to detail and code quality.
-      </p>
+        I am a software engineer who has been working for approximately 3 years. During this time, I have been involved in developing creative web applications and incorporating reliable APIs. I am a team player and enjoy collaborating with others to deliver top-notch solutions. I make it a point to thoroughly analyze requirements, design useful features, and keep up with the latest industry trends. With strong problem-solving abilities and effective communication skills, I am dedicated to achieving meaningful outcomes while paying close attention to detail and maintaining high standards of code quality.      </p>
     </div>
   );
 };
