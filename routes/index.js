77b5fb4ce01bbcbe96e20d98693cd90cd683e121
Romain <romainyvernes@gmail.com>
2023-08-15 13:40:37 -0700

    Remove server related files

--- routes/index.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const contactController = require('../controllers/contactController');
-
-router.post('/', contactController.sendEmail);
-
-module.exports = router;
