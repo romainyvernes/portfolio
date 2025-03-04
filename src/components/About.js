8d2ce00c840afa8037af23982ca030217d0dae07
Romain <65140547+romainyvernes@users.noreply.github.com>
2025-03-04 00:40:15 -0800

    Update About section description

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,7 @@ const About = () => {
         <FontAwesomeIcon icon={['far', 'user']} />
       </div>
       <p className="description">
-        I am a software engineer who has been working for approximately 3 years. During this time, I have been involved in developing creative web applications and incorporating reliable APIs. I am a team player and enjoy collaborating with others to deliver top-notch solutions. I make it a point to thoroughly analyze requirements, design useful features, and keep up with the latest industry trends. With strong problem-solving abilities and effective communication skills, I am dedicated to achieving meaningful outcomes while paying close attention to detail and maintaining high standards of code quality.      </p>
+        With three years of experience under my belt, I have developed dozens of creative web applications and incorporated as many APIs. I am a team player and enjoy collaborating with others to deliver top-notch solutions. I make it a point to thoroughly analyze requirements, design useful features, and keep up with the latest industry trends. With strong problem-solving abilities and effective communication skills, I am dedicated to achieving meaningful outcomes while paying close attention to detail and maintaining high standards of code quality.      </p>
     </div>
   );
 };
